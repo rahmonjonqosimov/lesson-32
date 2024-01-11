931ce51eae85d8973f251f0b170b8a3dcc3b999d
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-01-12 00:30:19 +0500

    header and footer

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//RESPONSIVE NAVBAR /////////////////////////////////////
+function toggleNavbar() {
+  document.getElementById("navbar-responsive").classList.toggle("open");
+}
+
+document.getElementById("menu-btn").addEventListener("click", toggleNavbar);
+// NAVBAR SHRINK ////////////////////////////////////
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+// BACTOP ///////////////////////////////////////////////
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = "20px";
+  } else {
+    backtop.style.bottom = "-50px";
+  }
+}
